da1adaac457d98c741c7e0e37042e0aa619170b4
Raihan_MSN <13515128@std.stei.itb.ac.id>
2019-09-28 22:18:57 +0700

    progressing transaction, changing branch

--- src/js/TransactionPage.js
+++ src/js/TransactionPage.js
@@ -11,91 +11,82 @@ function showHistory() {
     request.send();
 
     request.onload = () => {
-        let temp = request.jsonlist;
-        if (typeof temp !== undefined && !null) {
-            let response = JSON.parse(temp);
-        } else {
-            alert("type undef or empty");
-        }
+   
+        let temp = JSON.parse(request.response);
 
-        if (response == "No movies at the moment") {
-            // Action if there's no movie
-            alert("You haven't watch any movies. watch some!");
-        } else {
+        for (let i = 0; i < (temp.length); i++) {
 
-            for (let i = 0; i < (response.length - 1); i++) {
+            let container = document.createElement('div');
+            container.className = "movie-item";
 
-                let container = $("<div>");
-                container.className = "movie-item";
+            let img = document.createElement('img')
+            img.className = "movie-image";
+            img.src = temp[i].photo_link;
 
-                let img = $("<img>")
-                img.className = "movie-image";
-                img.src = "..data/photos/" + response[i].photo_link;
+            let detail = document.createElement('div');
+            detail.className = "movie-details";
 
-                let detail = $("<div>");
-                detail.className = "movie-details";
-
-                let title = $("<div>");
-                title.className = "details-title";
-                title.innerHTML = response[i].title ;
+            let title = document.createElement('div');
+            title.className = "details-title";
+            title.innerHTML = temp[i].title ;
 
 
-                let datetimecon = $("<span>");
-                datetimecon.className = "details-details";
+            let datetimecon = document.createElement('span');
+            datetimecon.className = "details-details";
                  
-                let datetime = $("<span>");
-                //datetime.className = "details-details";
-                datetime.innerHTML = response[i].date + " " + response[i].time ;
+            let datetime = document.createElement('span');
+            //datetime.className = "details-details";
+            datetime.innerHTML = temp[i].date + " " + temp[i].time ;
 
-                let schedule = $("<span>");
-                schedule.className = "blue-text";
-                schedule.innerHTML = "Schedule: ";
+            let schedule = document.createElement('span');
+            schedule.className = "blue-text";
+            schedule.innerHTML = "Schedule: ";
                  
-                let buttoncon = $("<form>");
-                buttoncon.className = "details-button";
-                buttoncon.action = "../php/ReviewPage.php";
-                buttoncon.method = "get";
+            let buttoncon = document.createElement('form');
+            buttoncon.className = "details-button";
+            buttoncon.action = "../php/ReviewPage.php";
+            buttoncon.method = "get";
 
-                let delbut = $("<button>");
-                delbut.className = "button-delete";
-                delbut.type = "submit";
-                delbut.innerHTML = "Delete Review";
-                delbut.value = -1;
-                delbut.name = "button";
+            let delbut = document.createElement('button');
+            delbut.className = "button-delete";
+            delbut.type = "submit";
+            delbut.innerHTML = "Delete Review";
+            delbut.value = -1;
+            delbut.name = "button";
 
-                let edbut = $("<button>");
-                edbut.className = "button-edit";
-                edbut.type = "submit";
-                edbut.innerHTML = "Edit Review";
-                edbut.value = reponse[i].title;
-                edbut.name = "button";
+            let edbut = document.createElement('button');
+            edbut.className = "button-edit";
+            edbut.type = "submit";
+            edbut.innerHTML = "Edit Review";
+            edbut.value = temp[i].title;
+            edbut.name = "button";
 
-                let adbut = $("<button>");
-                adbut.className = "button-add";
-                adbut.type = "submit";
-                adbut.innerHTML = "Add Review";
-                adbut.value = reponse[i].title;
-                adbut.name = "button";
+            let adbut = document.createElement('button');
+            adbut.className = "button-add";
+            adbut.type = "submit";
+            adbut.innerHTML = "Add Review";
+            adbut.value = temp[i].title;
+            adbut.name = "button";
 
-                if (response[i] != null) {
-                    buttoncon.appendChild(delbut);
-                    buttoncon.appendChild(edbut);
-                } else {
-                    if (Date.now > response[i].date) {
-                        buttoncon.appendChild(adbut);
-                    }
+            if (temp[i] != null) {
+                buttoncon.appendChild(delbut);
+                buttoncon.appendChild(edbut);
+            } else {
+                if (Date.now > temp[i].date) {
+                    buttoncon.appendChild(adbut);
                 }
-
-                datetimecon.appendChild(schedule);
-                datetimecon.appendChild(datetime);
-
-                detail.appendChild(title);
-                detail.appendChild(datetimecon);
-                dateil.appendChild(buttoncon);
-
-                container.appendChild(img);
-                container.appendChild(detail);
             }
+
+            datetimecon.appendChild(schedule);
+            datetimecon.appendChild(datetime);
+
+            detail.appendChild(title);
+            detail.appendChild(datetimecon);
+            detail.appendChild(buttoncon);
+
+            container.appendChild(img);
+            container.appendChild(detail);
         }
+        
     }
 }
