36f4c48dd0cc017664343c7be926139701b06fd9
jeremyarden <ardenjeremy@gmail.com>
2019-09-28 23:22:09 +0700

    add callback to database

---
+++ src/php/LoadReview.php
@@ -0,0 +1,30 @@
+<?php
+
+    /**Related files */
+    require "SqlConnection.php";
+    require "SqlUtility.php";
+
+    $idValue = null;
+
+    /**Request for movie review */
+    if (isset($_GET["id"])) {
+        $idValue = $_GET["id"];
+
+        $conn = openConnection();
+        $sql = "SELECT rating, review, username, profile_pic
+                FROM movie_user_table NATURAL JOIN user_table
+                WHERE movie_id = $id LIMIT 3";
+        $response = mysqli_query($conn, $sql);
+
+        if (!empty($response)) {
+            $result = array();
+
+            while ($r = mysqli_fetch_assoc($response)) {
+                $result[] = $r;
+            }
+
+            echo (json_encode($result));
+        }
+    }
+
+?>
