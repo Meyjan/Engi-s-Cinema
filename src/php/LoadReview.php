c6671a5319401c3ba91b8886d5d64e369cf0a8ba
Meyjan <saragih.meyer0821@gmail.com>
2019-09-29 09:19:10 +0700

    Added booking logic

--- src/php/LoadReview.php
+++ src/php/LoadReview.php
@@ -1,30 +1,30 @@
 <?php
 
-    /**Related files */
-    require "SqlConnection.php";
-    require "SqlUtility.php";
+/**Related files */
+require "SqlConnection.php";
+require "SqlUtility.php";
 
-    $idValue = null;
+$idValue = null;
 
-    /**Request for movie review */
-    if (isset($_GET["id"])) {
-        $idValue = $_GET["id"];
+/**Request for movie review */
+if (isset($_GET["id"])) {
+    $idValue = $_GET["id"];
 
-        $conn = openConnection();
-        $sql = "SELECT rating, review, username, profile_pic
-                FROM movie_user_table NATURAL JOIN user_table
-                WHERE movie_id = " . $idValue . " LIMIT 3";
-        $response = mysqli_query($conn, $sql);
+    $conn = openConnection();
+    $sql = "SELECT rating, review, username, profile_pic
+            FROM movie_user_table NATURAL JOIN user_table
+            WHERE review IS NOT NULL AND movie_id = " . $idValue . " LIMIT 3";
+    $response = mysqli_query($conn, $sql);
 
-        if (!empty($response)) {
-            $result = array();
+    if (!empty($response)) {
+        $result = array();
 
-            while ($r = mysqli_fetch_assoc($response)) {
-                $result[] = $r;
-            }
-
-            echo (json_encode($result));
+        while ($r = mysqli_fetch_assoc($response)) {
+            $result[] = $r;
         }
+
+        echo (json_encode($result));
     }
+}
 
 ?>
